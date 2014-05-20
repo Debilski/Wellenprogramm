987018a4d23c6681f0053b60bd68969f0e09b466
Rike-Benjamin Schuppner <rikebs@debilski.de>
2014-05-20 13:19:19 +0200

    BLD: Remove outdated helpers.

--- helper/set_dong_variables.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-echo "Setting up paths for dong computers."
-echo "It might be convinient to do this in an extra shell."
-export PATH=$PATH:/users/tsp/schuppner/local/bin
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/users/tsp/schuppner/local/lib
-export QTDIR=/users/tsp/schuppner/local
-
